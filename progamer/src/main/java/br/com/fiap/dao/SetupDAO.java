6d1937adc475bce924ee2090a1085c16ce3aab45
João Carlos Lima <joaocarloslima@me.com>
2021-04-14 09:29:32 -0300

    ajax e consultas

--- progamer/src/main/java/br/com/fiap/dao/SetupDAO.java
+++ progamer/src/main/java/br/com/fiap/dao/SetupDAO.java
@@ -1,25 +1,30 @@
 package br.com.fiap.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import br.com.fiap.model.Setup;
+import br.com.fiap.util.EntityManagerFacade;
 
 public class SetupDAO {
+	
+	private EntityManager manager = EntityManagerFacade.getEntityManager();
 
 	public void save(Setup setup) {
-		
-		EntityManagerFactory factory = 
-			Persistence.createEntityManagerFactory("progamer-persistence-unit");
-		
-		EntityManager manager = factory.createEntityManager();
-		
 		manager.getTransaction().begin();
 		manager.persist(setup);
 		manager.getTransaction().commit();
 		
 		manager.clear();
+		
+	}
+
+	public List<Setup> getAll() {
+		String jpql = "SELECT s FROM Setup s";
+		TypedQuery<Setup> query = manager.createQuery(jpql, Setup.class);
+		return query.getResultList();
 		
 	}
 
